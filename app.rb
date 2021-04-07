9c86c00d3f5d205c19476b073b97f6af1b7606de
Pavel Skripka <ckripka24@rambler.ru>
2021-04-07 16:30:46 +0300

    Add ActivRecord to visit

--- app.rb
+++ app.rb
@@ -1,4 +1,5 @@
 #encoding: utf-8
+
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
@@ -14,6 +15,35 @@ class Barber < ActiveRecord::Base
 
 end
 
+
 get '/' do
-	erb "Hello!!! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
+	@barbers = Barber.order "created_at DESC"
+	erb :index
+end
+
+get '/visit' do
+
+
+
+	#	<% @barb.each do |x| %>
+	#<option <%= @barber == x['name'] ? 'selected' : ''  %>> <%= x['name'] %> </option>
+	#<% end %>
+
+	
+	erb :visit
+end
+
+post '/visit' do
+
+	@user_name = params[:username]
+	@user_phone = params[:userphone]
+	@date_visit = params[:datevisit]
+	@time_visit = params[:timevisit]
+	@barber = params[:barbers]
+	@colorpicker = params[:colorpicker]
+
+
+  		Client.create :name => @user_name, :phone => @user_phone, :datestamp => @date_visit, :barber => @barber, :color => @colorpicker
+
+	erb :visit	
 end
